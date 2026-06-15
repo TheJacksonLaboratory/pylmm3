767b8aa6e154529ffb760eb4bf6de554511403ae
mattjvincent <matt.vincent@jax.org>
2026-06-15 11:01:39 -0400

    added dynamic get_version and __version__

--- pylmm3/__init__.py
+++ pylmm3/__init__.py
@@ -1,6 +1,26 @@
+from importlib.metadata import version, PackageNotFoundError
+
 from pylmm3.lmm import LMM
 from pylmm3.kinship import calculateKinship, NoVariantSNPsError
 from pylmm3.gwas_fast import runGWAS
 from pylmm3.input import load_snp_matrix
 
-__all__ = ["LMM", "calculateKinship", "NoVariantSNPsError", "runGWAS", "load_snp_matrix"]
+def get_version() -> str:
+    """Return the installed version of pylmm3."""
+    try:
+        return version("pylmm3")
+    except PackageNotFoundError:
+        # Returns this if the package isn't installed in the environment yet
+        return "unknown"
+
+__version__ = get_version()
+
+__all__ = [
+    "LMM",
+    "calculateKinship",
+    "NoVariantSNPsError",
+    "runGWAS",
+    "load_snp_matrix",
+    "get_version",
+    "__version__"
+]
